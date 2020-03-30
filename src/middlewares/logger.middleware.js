8e37b61ab6b4b273529223d1e2c6ebd030a861d3
taras.panasiuk <webdev.taras@gmail.com>
2020-03-30 21:01:42 +0300

    add transactionId to morgan logger

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,3 +1,16 @@
 const morgan = require('morgan')
+const { namespase } = require('../services/cls')
 
-module.exports = morgan('tiny')
+// module.exports = morgan('tiny')
+
+module.exports = morgan(function (tokens, req, res) {
+  const transactionId = namespase.get('transactionId')
+  return [
+    `[${transactionId}]`,
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
--- src/middlewares/transaction.middleware.js
+++ src/middlewares/transaction.middleware.js
@@ -1,8 +1,10 @@
 const { namespase } = require('../services/cls')
 const { v4: uuidv4 } = require('uuid')
-const logger = require('../logger')
 
 const requestIdMiddleware = (req, res, next) => {
+  namespase.bind(req)
+  namespase.bind(res)
+
   namespase.run(() => {
     const transactionId = uuidv4()
     namespase.set('transactionId', transactionId)
